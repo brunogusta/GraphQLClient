4a09db797c64d9226e6d347013c1f5ab6d9890d4
Bruno Gustavo <brugucolosil95@gmail.com>
2019-10-24 11:31:17 -0300

    added delete component

---
+++ web/src/pages/Users/components/Delete/index.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import {
+  Container,
+  Form,
+  FormContainer,
+  ResultsContainer,
+  TextError,
+  SearchButton
+} from './styles';
+
+export default function Delete() {
+  const [handleError, useHandleError] = useState({
+    emptyInputs: false
+  });
+
+  const NoFieldProvided = () => {
+    useHandleError({
+      emptyInputs: true
+    });
+
+    setTimeout(() => Reset(), 2000);
+  };
+
+  const Reset = () => {
+    useHandleError({
+      emptyInputs: false
+    });
+  };
+
+  const HandleSubmitValues = ({ id, email }) => {
+    if (id === email) {
+      return NoFieldProvided();
+    }
+  };
+
+  return (
+    <Container duration="1s">
+      <Formik
+        initialValues={{ id: '', email: '' }}
+        onSubmit={values => HandleSubmitValues(values)}
+        validationSchema={Yup.object().shape({
+          id: Yup.number().typeError('ID must be a number'),
+          email: Yup.string().email('E-mail is not valid')
+        })}
+        render={({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit
+        }) => (
+          <FormContainer>
+            <div>
+              <h3>Delete</h3>
+            </div>
+            <Form>
+              <input
+                type="input"
+                name="id"
+                placeholder="ID"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.id}
+              />
+              {errors.id && touched.id && <TextError>{errors.id}</TextError>}
+              <input
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {errors.email && touched.email && (
+                <TextError>{errors.email}</TextError>
+              )}
+              {handleError.emptyInputs && (
+                <TextError>{'No fields provided'}</TextError>
+              )}
+              <SearchButton type="submit" onClick={handleSubmit}>
+                <p>DELETE</p>
+              </SearchButton>
+            </Form>
+          </FormContainer>
+        )}
+      />
+      <ResultsContainer>
+        <div>
+          <h3>Results</h3>
+        </div>
+      </ResultsContainer>
+    </Container>
+  );
+}
