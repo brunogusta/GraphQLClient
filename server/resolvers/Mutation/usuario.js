3607ba3a355f460b5d4959bf6fec3f786481c52d
Bruno Gustavo <brugucolosil95@gmail.com>
2019-10-29 21:19:02 -0300

    added some errors handling to  backend

--- server/resolvers/Mutation/usuario.js
+++ server/resolvers/Mutation/usuario.js
@@ -1,113 +1,126 @@
-const bcrypt = require('bcrypt-nodejs')
-const db = require('../../config/db')
-const { perfil: obterPerfil } = require('../Query/perfil')
-const { usuario: obterUsuario } = require('../Query/usuario')
+const bcrypt = require('bcrypt-nodejs');
+const db = require('../../config/db');
+const { perfil: obterPerfil } = require('../Query/perfil');
+const { usuario: obterUsuario } = require('../Query/usuario');
 
 const mutations = {
-    registrarUsuario(_, { dados }) {
-        return mutations.novoUsuario(_, {
-            dados: {
-                nome: dados.nome,
-                email: dados.email,
-                senha: dados.senha,
-            }
-        })
-    },
-    async novoUsuario(_, { dados }, ctx) {
-        ctx && ctx.validarAdmin()
-        try {
-            const idsPerfis = []
+  async registrarUsuario(_, { dados }) {
+    const email = await db('usuarios')
+      .where({ email: dados.email })
+      .first();
 
-            if(!dados.perfis || !dados.perfis.length) {
-                dados.perfis = [{
-                    nome: 'comum'
-                }]
-            }
+    const name = await db('usuarios')
+      .where({ nome: dados.nome })
+      .first();
 
-            for(let filtro of dados.perfis) {
-                const perfil = await obterPerfil(_, {
-                    filtro
-                })
-                if(perfil) idsPerfis.push(perfil.id)
-            }
+    if (email) throw new Error('The entered email already exists.');
+    if (name) throw new Error('The entered name already exists.');
 
-            // criptografar a senha
-            const salt = bcrypt.genSaltSync()
-            dados.senha = bcrypt.hashSync(dados.senha, salt)
+    return mutations.novoUsuario(_, {
+      dados: {
+        nome: dados.nome,
+        email: dados.email,
+        senha: dados.senha
+      }
+    });
+  },
+  async novoUsuario(_, { dados }, ctx) {
+    ctx && ctx.validarAdmin();
+    try {
+      const idsPerfis = [];
 
-            delete dados.perfis
-            const [ id ] = await db('usuarios')
-                .insert(dados)
+      if (!dados.perfis || !dados.perfis.length) {
+        dados.perfis = [
+          {
+            nome: 'Comum'
+          }
+        ];
+      }
 
-            for(let perfil_id of idsPerfis) {
-                await db('usuarios_perfis')
-                    .insert({ perfil_id, usuario_id: id })
-            }
+      for (let filtro of dados.perfis) {
+        const perfil = await obterPerfil(_, {
+          filtro
+        });
+        if (perfil) idsPerfis.push(perfil.id);
+      }
 
-            return db('usuarios')
-                .where({ id }).first()
-        } catch(e) {
-            throw new Error(e.sqlMessage)
-        }
-    },
-    async excluirUsuario(_, args, ctx) {
-        ctx && ctx.validarAdmin()
-        try {
-            const usuario = await obterUsuario(_, args)
-            if(usuario) {
-                const { id } = usuario
-                await db('usuarios_perfis')
-                    .where({ usuario_id: id }).delete()
-                await db('usuarios')
-                    .where({ id }).delete()
-            }
-            return usuario
-        } catch(e) {
-            throw new Error(e.sqlMessage)
-        }
+      // criptografar a senha
+      const salt = bcrypt.genSaltSync();
+      dados.senha = bcrypt.hashSync(dados.senha, salt);
 
-    },
-    async alterarUsuario(_, { filtro, dados }, ctx) {
-        ctx && ctx.validarUsuarioFiltro(filtro)
-        try {
-            const usuario = await obterUsuario(_, { filtro })
-            if(usuario) {
-                const { id } = usuario
-                if(ctx.admin && dados.perfis) {
-                    await db('usuarios_perfis')
-                        .where({ usuario_id: id }).delete()
+      delete dados.perfis;
+      const [id] = await db('usuarios').insert(dados);
 
-                    for(let filtro of dados.perfis) {
-                        const perfil = await obterPerfil(_, {
-                            filtro
-                        })
-                        
-                        if(perfil) {
-                            await db('usuarios_perfis')
-                                .insert({
-                                    perfil_id: perfil.id,
-                                    usuario_id: id
-                                })
-                        }
-                    }
-                }
+      for (let perfil_id of idsPerfis) {
+        await db('usuarios_perfis').insert({ perfil_id, usuario_id: id });
+      }
 
-                if(dados.senha) {
-                    // criptografar a senha
-                    const salt = bcrypt.genSaltSync()
-                    dados.senha = bcrypt.hashSync(dados.senha, salt)
-                }
-
-                delete dados.perfis
-                await db('usuarios')
-                    .where({ id })
-                    .update(dados)
-            }
-            return !usuario ? null : { ...usuario, ...dados }
-        } catch(e) {
-            throw new Error(e)
-        }
+      return db('usuarios')
+        .where({ id })
+        .first();
+    } catch (e) {
+      throw new Error(e.sqlMessage);
     }
-}
+  },
+  async excluirUsuario(_, args, ctx) {
+    ctx && ctx.validarAdmin();
+    try {
+      const usuario = await obterUsuario(_, args);
+      if (usuario) {
+        const { id } = usuario;
+        await db('usuarios_perfis')
+          .where({ usuario_id: id })
+          .delete();
+        await db('usuarios')
+          .where({ id })
+          .delete();
+      }
+      return usuario;
+    } catch (e) {
+      throw new Error(e.sqlMessage);
+    }
+  },
+  async alterarUsuario(_, { filtro, dados }, ctx) {
+    ctx && ctx.validarUsuarioFiltro(filtro);
+    try {
+      const usuario = await obterUsuario(_, { filtro });
+      if (usuario) {
+        const { id } = usuario;
+        if (ctx.admin && dados.perfis) {
+          await db('usuarios_perfis')
+            .where({ usuario_id: id })
+            .delete();
 
-module.exports = mutations
+          for (let filtro of dados.perfis) {
+            const perfil = await obterPerfil(_, {
+              filtro
+            });
+
+            if (perfil) {
+              await db('usuarios_perfis').insert({
+                perfil_id: perfil.id,
+                usuario_id: id
+              });
+            }
+          }
+        }
+
+        if (dados.senha) {
+          // criptografar a senha
+          const salt = bcrypt.genSaltSync();
+          dados.senha = bcrypt.hashSync(dados.senha, salt);
+        }
+
+        delete dados.perfis;
+        await db('usuarios')
+          .where({ id })
+          .update(dados);
+      }
+      return !usuario ? null : { ...usuario, ...dados };
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+};
+
+module.exports = mutations;
